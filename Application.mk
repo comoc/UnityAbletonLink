20ab106f8656dc3b103b9fcc7de19f571d2d3374
Akihiro Komori <akihiro@Akihiros-MacBook-Air.local>
2021-04-23 09:37:14 +0900

    fix ndk-build issue on M1

--- Application.mk
+++ Application.mk
@@ -1,5 +1,5 @@
 APP_OPTIM        := release
-APP_ABI          := armeabi-v7a
+APP_ABI          := armeabi-v7a arm64-v8a
 APP_PLATFORM     := android-16
 APP_BUILD_SCRIPT := Android.mk
 APP_STL          := c++_static
